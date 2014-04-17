cb7e1c1c99640715f67b2a37c1092f8a257dadcb
Gaurav <igauravsehrawat@gmail.com>
2014-04-17 13:15:25 +0530

    meta info on anagram.rb

---
+++ hackerrank/anagram.rb
@@ -0,0 +1,5 @@
+#break .. 
+#hash way in ruby is easy with inject(&:&) 
+#to find the comparision of various common in hashes.
+#hash is intialized with non numberic form 
+# + won't work if there the type does not matches. not automatic converison
